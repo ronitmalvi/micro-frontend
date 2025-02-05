4e81550bbcb7daa054d83d31d2cffe9f986a4df0
Ronit <malvironit1103@gmail.com>
2025-02-05 21:48:13 +0530

    commit

--- email/vite.config.js
+++ email/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       exposes: {
         './App': './src/App'
       },
-      shared: ['react', 'react-dom']
+      shared: ['react', 'react-dom','react-gauge-chart']
     })
   ],
   build: {
